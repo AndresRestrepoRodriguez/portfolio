771f5243bf715e73eb00efcfa63f535ecb2b05d1
andres restrepo <aorestrepor@correo.udistrital.edu.co>
2023-01-20 09:21:07 -0500

    Add unet segmentation project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -35,16 +35,20 @@ export default function Home() {
             <Divider size='md' borderColor={'#2D3748'} />
             <Divider size='md' borderColor={'#2D3748'} />
             <Flex direction={["column", "column", "row"]} pt={6}>
+            <Project image_url={'/unet_project.png'} title={'Unet Segmentation - MRI'} source_link='https://github.com/AndresRestrepoRodriguez/MRI_Unet_Segmentation' labels={["Deep Learning", "Computer Vision", "Image Segmentation"]}/>
             <Project image_url={'/dlops.png'} title={'DlOps'} source_link='https://github.com/AndresRestrepoRodriguez/dlops' labels={["Deep Learning", "Python", "MlOps"]}/>
+            </Flex>
+            <Flex direction={["column", "column", "row"]} pt={6}>
             <Project image_url={'portfolio.png'} title={'Portfolio'} source_link='https://github.com/AndresRestrepoRodriguez/portfolio' labels={["NextJS", "ChakraUi", "Web"]}/>
-            </Flex>
-            <Flex direction={["column", "column", "row"]} pt={6}>
             <Project image_url={'/computer_vision.png'} title={'Computer Vision Notebooks'} source_link='https://github.com/AndresRestrepoRodriguez/computer_vision' labels={["Deep Learning", "Python", "Computer Vision"]}/>
-            <Project image_url={'/entropy.jpg'} title={'Entropy - Selection APP'} source_link='https://github.com/AndresRestrepoRodriguez/Entropy-SelApp' labels={["Python", "Feature Engineering", "Web"]}/>
             </Flex>
             <Flex direction={["column", "column", "row"]} pt={6}>
+            <Project image_url={'/entropy.jpg'} title={'Entropy - Selection APP'} source_link='https://github.com/AndresRestrepoRodriguez/Entropy-SelApp' labels={["Python", "Feature Engineering", "Web"]}/>
             <Project image_url={'/aerial.png'} title={'CNNs Aerial Image Classification'} source_link='https://github.com/AndresRestrepoRodriguez/Comparison-CNN-Models-Aerial-Image-Classification' labels={["Deep Learning", "Python", "Computer Vision"]}/>
+            </Flex>
+            <Flex direction={["column", "column", "row"]} pt={6}>
             <Project image_url={'/sallfus.png'} title={'Satellite Images Fusion CPU/GPU'} source_link='https://github.com/AndresRestrepoRodriguez/satellite_images_fusion' labels={["Python", "Image Processing", "GPU"]}/>
+            <Project image_url={'/coming_soon.jpg'} title={'Coming Soon'} source_link='#' labels={["unkown", "unknown", "unkown"]}/>
             </Flex>
           </Box>
         </Box>
