7d04fa47968093b58dc45d77a2a5144353a20483
andres restrepo <aorestrepor@correo.udistrital.edu.co>
2023-01-24 21:20:48 -0500

    Add hotel recommender project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -39,16 +39,16 @@ export default function Home() {
             <Project image_url={'/dlops.png'} title={'DlOps'} source_link='https://github.com/AndresRestrepoRodriguez/dlops' labels={["Deep Learning", "Python", "MlOps"]}/>
             </Flex>
             <Flex direction={["column", "column", "row"]} pt={6}>
-            <Project image_url={'portfolio.png'} title={'Portfolio'} source_link='https://github.com/AndresRestrepoRodriguez/portfolio' labels={["NextJS", "ChakraUi", "Web"]}/>
+            <Project image_url={'/hotel_recommender.png'} title={'Simple Hotel Recommender'} source_link='https://github.com/AndresRestrepoRodriguez/hotels_recommender' labels={["Text Processing", "NLP", "Python"]}/>
             <Project image_url={'/computer_vision.png'} title={'Computer Vision Notebooks'} source_link='https://github.com/AndresRestrepoRodriguez/computer_vision' labels={["Deep Learning", "Python", "Computer Vision"]}/>
             </Flex>
             <Flex direction={["column", "column", "row"]} pt={6}>
-            <Project image_url={'/entropy.jpg'} title={'Entropy - Selection APP'} source_link='https://github.com/AndresRestrepoRodriguez/Entropy-SelApp' labels={["Python", "Feature Engineering", "Web"]}/>
+            <Project image_url={'portfolio.png'} title={'Portfolio'} source_link='https://github.com/AndresRestrepoRodriguez/portfolio' labels={["NextJS", "ChakraUi", "Web"]}/>
             <Project image_url={'/aerial.png'} title={'CNNs Aerial Image Classification'} source_link='https://github.com/AndresRestrepoRodriguez/Comparison-CNN-Models-Aerial-Image-Classification' labels={["Deep Learning", "Python", "Computer Vision"]}/>
             </Flex>
             <Flex direction={["column", "column", "row"]} pt={6}>
             <Project image_url={'/sallfus.png'} title={'Satellite Images Fusion CPU/GPU'} source_link='https://github.com/AndresRestrepoRodriguez/satellite_images_fusion' labels={["Python", "Image Processing", "GPU"]}/>
-            <Project image_url={'/coming_soon.jpg'} title={'Coming Soon'} source_link='#' labels={["unkown", "unknown", "unkown"]}/>
+            <Project image_url={'/entropy.jpg'} title={'Entropy - Selection APP'} source_link='https://github.com/AndresRestrepoRodriguez/Entropy-SelApp' labels={["Python", "Feature Engineering", "Web"]}/>            {/*<Project image_url={'/coming_soon.jpg'} title={'Coming Soon'} source_link='#' labels={["unkown", "unknown", "unkown"]}/>*/}
             </Flex>
           </Box>
         </Box>
